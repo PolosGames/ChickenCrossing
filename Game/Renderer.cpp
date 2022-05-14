197d7cfc5f6befab4d0153f3ab611fda334859c1
Kayra <44715451+kayraurfali@users.noreply.github.com>
2022-05-14 19:00:34 +0300

    Renderer updated to use the TransformComponent

--- Game/Renderer.cpp
+++ Game/Renderer.cpp
@@ -32,14 +32,16 @@ void InitializeRenderer()
     g_Resources->texture_shader->SetMat("u_Proj"_sid, k_Projection);
 }
 
-void RenderTexture(pl::Texture const& texture, glm::vec3 const& position, 
-                    glm::vec2 const& dimensions, float rotation)
+void RenderTexture(pl::Texture const& texture, TransformComponent const& transform)
 {
     glm::mat4 model{ 1.0 };
-    glm::vec2 size = (dimensions.x == 0.0f && dimensions.y == 0.0f) ? glm::vec2{ texture.width, texture.height } : dimensions;
+    glm::vec2 const size = [&transform, &texture]() {
+        return (transform.size.x == 0.0f && transform.size.y == 0.0f) ? 
+            glm::vec2{ texture.width, texture.height } :  glm::vec2{ transform.size.x, transform.size.y };
+    }();
 
-    model = glm::translate(model, glm::vec3{ position });
-    model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 0.0f, 1.0f));
+    model = glm::translate(model, glm::vec3{ transform.position });
+    model = glm::rotate(model, glm::radians(transform.rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
     model = glm::scale(model, glm::vec3(size, 1.0f));
 
     g_Resources->texture_shader->Use();
--- Game/Renderer.h
+++ Game/Renderer.h
@@ -5,13 +5,14 @@
 #include <polos/context/texture.h>
 #include <polos/context/vao.h>
 
+#include "Components.h"
+
 namespace constants
 {
     extern const glm::mat4 k_Projection;
 }
 
 void InitializeRenderer();
-void RenderTexture(pl::Texture const& texture, glm::vec3 const& position, 
-                    glm::vec2 const& dimensions = glm::vec2{0.0f}, float rotation = 0.0f);
+void RenderTexture(pl::Texture const& texture, TransformComponent const& transform);
 
 #endif /* RENDERER_H_ */
